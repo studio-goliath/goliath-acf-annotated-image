a4820e3d53529cb19aec122e487a6079199582c9
ArnaudBan <abanvillet@studio-goliath.com>
2016-09-14 16:42:43 +0200

    Always show the textearea to edit annotations

--- assets/js/goliath-annotated-image.js
+++ assets/js/goliath-annotated-image.js
@@ -1,65 +1,63 @@
 (function($) {
     $.fn.annotatedImage = function() {
         var fieldName = 'notes';
-        var idx = 1;
         var animating = false;
+        var idx = 0;
         var hndShowAnimate;
         var animTimeFade = 300;
         var animTimeBetweenNotes = 300;
         var initiated = false;
 
         var displayRelatedTextarea = function($note) {
-            $note.parent().parent().find('.textarea').hide();
-            var $relatedTextarea = $note.parent().find('.textarea');
+
             if (initiated) {
-                $relatedTextarea.show().find('textarea').focus();
+                var noteId = $note.attr('id').substring( 5 );
+                var $relatedTextarea = $('textarea[data-note-id="'+ noteId +'"]');
+                $relatedTextarea.focus();
             }
         };
 
-        var attachNoteEvents = function(editable, $noteHtml) {
-            if (editable) {
-                var $textarea = $noteHtml.find('.textarea');
-                $textarea.find('textarea')
-                    .keyup(function() {
-                        updateNoteText($noteHtml, $(this).val());
-                    })
-                    .blur(function() {
-                        $textarea.hide();
-                    });
-
-                $textarea.hide();
-                var $note = $noteHtml.find('span.note');
-                $note.click(function() {
-                    displayRelatedTextarea($(this));
+        var attachNoteEvents = function( $container) {
+            var $textarea = $container.find('textarea');
+            $textarea
+                .on({
+                    keyup: function() {
+                        updateNoteText( $(this) );
+                    },
+                    focusin: function(){
+                        var noteId = $(this).data('note-id');
+                        $('#note-' + noteId).addClass('current');
+                    },
+                    focusout: function(){
+                        var noteId = $(this).data('note-id');
+                        $('#note-' + noteId).removeClass('current');
+                    }
                 });
-            }
+
+
+            var $note = $container.find('span.note');
+            $note.click(function() {
+                displayRelatedTextarea($(this));
+            });
         };
 
-        var updateNoteText = function($parent, text) {
-            var $text = $parent.find('span.text');
-            $text.html((text)?text.replace(/\n/g, "<br />"):'');
-            $text.css('margin-left', (-Math.ceil($text.width() / 2)).toString() + 'px');
+        var updateNoteText = function( $textarea ) {
+
+
+            var text = $textarea.val();
+            var noteId = $textarea.data('note-id');
+            var $note = $('#note-' + noteId + ' .text' );
+
+            $note.html((text)?text.replace(/\n/g, "<br />"):'');
+            $note.css('margin-left', (-Math.ceil($note.width() / 2)).toString() + 'px');
         };
 
-        var getNoteHtml = function(editable, relX, relY, text, idx) {
+        var getNoteHtml = function( relX, relY, text, id) {
             var noteHtml = '';
 
-            noteHtml = noteHtml + '<span>';
-            noteHtml = noteHtml + '<span class="note" style="left:' + relX.toString() + '%; top:' + relY.toString() + '%">';
+            noteHtml = noteHtml + '<span class="note" id="note-'+ id +'" style="left:' + relX.toString() + '%; top:' + relY.toString() + '%">';
             noteHtml = noteHtml + '<span class="marker"></span>';
             noteHtml = noteHtml + '<span class="text"></span>';
-            noteHtml = noteHtml + '</span>';
-            if (editable) {
-                noteHtml = noteHtml + '<input type="hidden" name="' + fieldName + '[notes][' + idx.toString() + '][x]" value="' + relX.toString() + '" />';
-                noteHtml = noteHtml + '<input type="hidden" name="' + fieldName + '[notes][' + idx.toString() + '][y]" value="' + relY.toString() + '" />';
-                noteHtml = noteHtml + '<div class="textarea">';
-                noteHtml = noteHtml + '<div class="acf-label"><label for="' + fieldName + '_notes_' + idx.toString() + '_text">Texte de la note</label></div>';
-                noteHtml = noteHtml + '<div class="acf-input">';
-                noteHtml = noteHtml + '<textarea id="' + fieldName + '_notes_' + idx.toString() + '_text" name="' + fieldName + '[notes][' + idx.toString() + '][t]" class="note-input">' + text + '</textarea>';
-                noteHtml = noteHtml + '</div>';
-                noteHtml = noteHtml + '</div>';
-            }
-
             noteHtml = noteHtml + '</span>';
 
             var $noteHtml = $(noteHtml);
@@ -67,18 +65,13 @@
             return $noteHtml;
         };
 
-        var addNote = function($container, relX, relY, text) {
-            var editable = $container.hasClass('editable');
+        var addNote = function($container, relX, relY, text, id) {
 
-            var $noteHtml = getNoteHtml(editable, relX, relY, text, idx);
+            var $noteHtml = getNoteHtml( relX, relY, text, id);
 
-            $container.append($noteHtml);
+            $('.annotated-image', $container).append($noteHtml);
 
-            updateNoteText($noteHtml, text);
-            attachNoteEvents(editable, $noteHtml);
             displayRelatedTextarea($noteHtml);
-
-            idx++;
         };
 
         var showNextNote = function($container) {
@@ -131,6 +124,24 @@
             });
         };
 
+        var addInputNote = function( $container, relX, relY, text, idx ){
+
+            var noteInputHtml = '';
+
+            noteInputHtml += '<input type="hidden" name="' + fieldName + '[notes][' + idx.toString() + '][x]" value="' + relX.toString() + '" />';
+            noteInputHtml += '<input type="hidden" name="' + fieldName + '[notes][' + idx.toString() + '][y]" value="' + relY.toString() + '" />';
+            noteInputHtml += '<div class="textarea">';
+            noteInputHtml += '<div class="acf-label"><label for="' + fieldName + '_notes_' + idx.toString() + '_text">Texte de la note n°<span class="note-id">'+ idx +'</span></label></div>';
+            noteInputHtml += '<div class="acf-input">';
+            noteInputHtml += '<textarea id="' + fieldName + '_notes_' + idx.toString() + '_text" name="' + fieldName + '[notes][' + idx.toString() + '][t]" class="note-input" data-note-id="'+ idx +'">' + text + '</textarea>';
+            noteInputHtml += '</div>';
+            noteInputHtml += '</div>';
+
+            $container.append( noteInputHtml );
+
+            updateNoteText( $( 'textarea', noteInputHtml ) );
+        }
+
         var loadData = function($container) {
             var notes = $container.data('annotations');
             if (!notes) {
@@ -140,20 +151,41 @@
             for (var i in notes) {
                 if (notes.hasOwnProperty(i)) {
                     var note = notes[i];
-                    addNote($container, note.x, note.y, note.t);
+                    addNote($container, note.x, note.y, note.t, i);
                 }
             }
+
+            var editable = $container.hasClass('editable');
+
+            if( editable ){
+
+                for ( i in notes) {
+                    if (notes.hasOwnProperty(i)) {
+                        note = notes[i];
+                        addInputNote($container, note.x, note.y, note.t, i);
+                    }
+                }
+
+                attachNoteEvents( $container);
+            }
+
+            idx = Object.keys(notes).length + 1;
         };
 
         return this.each(function() {
 
             var $image = $(this);
 
-            var $container =
-                $('<div></div>')
-                    .attr('class', $image.attr('class'))
-                    .attr('data-annotations', $image.attr('data-annotations'))
-                    .attr('data-fieldname', $image.attr('data-fieldname'));
+            var $imageDiv = $('<div class="annotated-image"/>');
+
+            var $container = $('<div class="annotated-image-wrapper" />')
+                .addClass( $image.attr('class') )
+                .removeClass('annotated-image')
+                .append( $imageDiv )
+
+            for( var dataName in $image.data() ){
+                $container.attr( 'data-' + dataName, $image.attr( 'data-' + dataName ) );
+            }
 
             if ($container.data('fieldname')) {
                 fieldName = $container.data('fieldname');
@@ -163,16 +195,24 @@
             $image.attr('data-annotations', null);
             $image.attr('data-fieldname', null);
 
-            $container = $image.wrap($container).parent();
+            $image.after( $container );
+            $imageDiv.append( $image );
+
+            var $acfModifyAndDeletedLink = $container.next().remove();
+            $container.before( $acfModifyAndDeletedLink );
 
             loadData($container);
 
             if ($container.hasClass('editable')) {
+
                 $image.click(function(e) {
                     var parentOffset = $image.offset();
                     var relX = (e.pageX - parentOffset.left) / $image.width() * 100;
                     var relY = (e.pageY - parentOffset.top) / $image.height() * 100;
-                    addNote($container, relX, relY, '');
+                    addInputNote($container, relX, relY, '', idx);
+                    addNote($container, relX, relY, '', idx);
+                    attachNoteEvents( $container);
+                    idx++;
                 });
             } else {
                 animateNotes($container);
